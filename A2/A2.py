ee5d4b9e78a27ac8bce2aaed15efe133486c5845
Aaron <aaronde314@gmail.com>
2019-11-02 21:18:43 -0400

    fixed bugs and added a print board

--- A2/A2.py
+++ A2/A2.py
@@ -1,3 +1,5 @@
+from queue import Queue
+
 class Node:
     def __init__(self,row,col,value=None, domain=range(1, 10)):
         self.row = row
@@ -11,22 +13,20 @@ class Node:
     def __repr__(self):
         return '({}, {}, {}, {})'.format(self.row, self.col, self.value, self.domain)
 
-
-
-def addNeighbhors(queue,node, puzzle):
-    currentRow = node.i
-    currentCollumn = node.j 
+def addNeighbours(queue,node, puzzle):
+    currentRow = node.row
+    currentCollumn = node.col
     #grab neighbhors in nodes row
     i = currentRow
     for j in range(9): 
-        if (currentRow != i and currentCollumn != j):
-            queue.add(puzzle[i][j])
+        if not (currentRow == i and currentCollumn == j):
+            queue.put(puzzle[i][j])
 
     #grab neighbhors in nodes column
     j = currentCollumn
     for i in range(9): 
-        if (currentRow != i and currentCollumn != j):
-            queue.add(puzzle[i][j])
+        if not (currentRow == i and currentCollumn == j):
+            queue.put(puzzle[i][j])
 
     #grab neighbhors in box
     row = (currentRow // 3) * 3
@@ -34,8 +34,8 @@ def addNeighbhors(queue,node, puzzle):
 
     for i in range(3) :
         for j in range(3) :
-            if (currentRow != row +i and currentCollumn != col+ j):
-                queue.add(puzzle[row +i][col+ j])
+            if not (currentRow == row +i and currentCollumn == col+ j):
+                queue.put(puzzle[row +i][col+ j])
    
 def loadPuzzle(file='./puzzles/easy.csv', num=1, header=True):
 
@@ -56,13 +56,17 @@ def loadPuzzle(file='./puzzles/easy.csv', num=1, header=True):
 
             puzzle = []
             for j in range(9):
+                row = []
                 for k in range(9):
-                    puzzle.append(Node(j, k, None if puzzleAndSol[0][j*(9) + k] == '.' else puzzleAndSol[0][j*(9) + k]))
+                    row.append(Node(j, k, None if puzzleAndSol[0][j*(9) + k] == '.' else puzzleAndSol[0][j*(9) + k]))
+                puzzle.append(row)
 
             solution = []
             for j in range(9):
+                row = []
                 for k in range(9):
-                    solution.append(Node(j, k, None if puzzleAndSol[1][j*(9) + k] == '.' else puzzleAndSol[1][j*(9) + k]))
+                    row.append(Node(j, k, None if puzzleAndSol[1][j*(9) + k] == '.' else puzzleAndSol[1][j*(9) + k]))
+                solution.append(row)
 
             if num > 1:
                 puzzles.append(puzzle)
@@ -72,6 +76,24 @@ def loadPuzzle(file='./puzzles/easy.csv', num=1, header=True):
     else:
         return puzzle, solution
 
+def print_board(puzzle):
+
+    print('- ' * 13)
+    for row in puzzle:
+        print('|', end=' ')
+        for col in row:
+            print(col.value if col.value else '.', end=' ')
+            if col.col % 3 == 2:
+                print('|', end=' ')
+        print()
+        if col.row % 3 == 2:
+            print('- ' * 13)
+
 if __name__ == "__main__":
-    # print(loadPuzzle())
-    pass
+    q = Queue()
+    p, s = loadPuzzle()
+    # addNeighbours(q, p[0][0], p)
+    # print(q.qsize())
+    # while not q.empty():
+    #     print(q.get_nowait())
+    print_board(p)
