cab0d09708356c2abec588984ec607a43422b3c6
Aaron <aaronde314@gmail.com>
2019-11-05 18:26:35 -0500

    Got the backtracking working, just need to add one of the heuristics

--- A2/A2.py
+++ A2/A2.py
@@ -1,4 +1,5 @@
 from queue import Queue
+from copy import deepcopy
 
 class Node:
     def __init__(self,row,col,value=None, domain=range(1, 10)):
@@ -7,7 +8,7 @@ class Node:
         if value is not None: 
             self.value = int(value)
         else : 
-            self.value =value
+            self.value = value
         self.domain = list(domain)
 
     def __str__(self):
@@ -104,6 +105,7 @@ def AC3(puzzle):
     #checking if puzzle is solved
     noneValueFound= False
     while (i < 9 and not noneValueFound):
+        j = 0
         while(j < 9 and not noneValueFound):
             if puzzle[i][j].value is None: 
                 noneValueFound = True
@@ -114,8 +116,20 @@ def AC3(puzzle):
 
 
 
-def loadPuzzle(file='./puzzles/easy.csv', num=1, header=True):
-
+def loadPuzzle(file='./puzzles/easy.csv', num=1, header=True, start=0):
+    '''
+    Loads a puzzle from a file
+    ---------------------------
+    Params (optional):
+        file: the file to load, defaults to puzzle/easy.csv
+        num: the number of puzzles to load, defaults to 1
+        header: if the file has a header or not, defaults to True
+        start: what line to start reading the puzzle from
+    returns:
+        puzzle: a 2d array of Nodes representing the puzzle
+        solution: a 2d array of Nodes representing the solution of the puzzle
+        Note: if num > 1 will return an array of puzzles and and array of solutions
+    '''
     with open(file, 'r') as f:
         
         if num > 1:
@@ -124,9 +138,13 @@ def loadPuzzle(file='./puzzles/easy.csv', num=1, header=True):
 
         for i, line in enumerate(f):
             if header and i == 0:
+                start += 1
+                continue
+
+            if i < start:
                 continue
         
-            if i > num:
+            if i > start + num:
                 break
 
             puzzleAndSol = line.split(',')
@@ -153,13 +171,219 @@ def loadPuzzle(file='./puzzles/easy.csv', num=1, header=True):
     else:
         return puzzle, solution
 
-def print_board(puzzle):
+def backtrackSearch(puzzle):
+    '''
+    Performs a backtracking search on a csp sudoku
+    ---------------------------
+    Param:
+        puzzle: A 2d array of Node objects
+    returns:
+        A solved sudoku puzzle
+        A boolean of if the puzzle is solved or not
+    '''
+
+    # Find the starting node based on the degree heuristic
+    # ie selecting the node with largest amount of constraints
+    firstNode = None
+    for row in puzzle:
+        for node in row:
+            if node.value is None and (firstNode is None or len(firstNode.domain) < len(node.domain)):
+                firstNode = node
+
+    # Starts the backtracking
+    return backtrack(puzzle, firstNode.row, firstNode.col)
+
+def backtrack(puzzle, row, col):
+    '''
+    Auxiliary Performs a backtracking search on a csp sudoku
+    ---------------------------
+    Params:
+        puzzle: A 2d array of Node objects
+        row: The row of the current node
+        col: The col of the current node
+    returns:
+        A solved sudoku puzzle
+        A boolean for if the puzzle is solved or not
+    '''
+    
+    # Check if the puzzle is finished, if it is we are done
+    # and collapse the call stack
+    if complete(puzzle):
+        return puzzle, True
+
+    # Get the order of the domain using the
+    # least consraining value heuristic
+    domainOrder = order(row, col, puzzle)
+
+    for value in domainOrder:
+
+        # Check if the current value in the domain is consistant
+        # with the constraints of the sudoku
+        # Should always be consisitant
+        if valid(puzzle, row, col, value):
+            
+            # Store a copy of the current state for
+            # the backtracking
+            state = deepcopy(puzzle)
+
+            # Update the value of the current node
+            puzzle[row][col].value = value
+
+            # Make the updated puzzle arc consistant
+            puzzle, completed, noSolution = AC3(puzzle)
+
+            # If the puzzle still has a solution
+            # We can continue, Otherwise we move on 
+            if not noSolution:
+                if not completed:
+                    # Figure out which node we should check next using
+                    # MRV
+                    nextNode = getNextNode(puzzle)
+
+                    # Call the next node
+                    puzzle, completed = backtrack(puzzle, nextNode.row, nextNode.col)
+                    
+                    # We returned from the backtracking
+                    # if completed then we are done
+                    # collapse the call stack
+                    if completed:
+                        return puzzle, completed
+                else:
+                    return puzzle, completed
+        # we returned from the backtracking
+        # or the value is invalid
+        # so restore the starting state
+        # remove the value from the domain
+        # and continue to the next value in the domain
+        puzzle = state
+        puzzle[row][col].domain.remove(value)
+    puzzle[row][col].value = None
+
+    # This value had no values in its domain that worked
+    # Backtrack to previous node
+    return puzzle, False
+
+def valid(puzzle, row, col, value):
+    '''
+    Checks if a value is valid with the contraint
+    ---------------------------
+    Params:
+        puzzle: A 2d array of Node objects
+        row: The row of the current node
+        col: The col of the current node
+        value: The value you are checking that works
+    returns:
+        True if the value is valid, false otherwise
+    '''
+    node = puzzle[row][col]
+
+    # Row
+    col = node.col
+    for row in range(9):
+        if puzzle[row][col].value == value:
+            return False
+    
+    # Column
+    row = node.row
+    for col in range(9):
+        if puzzle[row][col].value == value:
+            return False
+    # Box
+    row = (node.row // 3) * 3
+    col = (node.col // 3) * 3
+    for i in range(3):
+        for j in range(3):
+            if puzzle[row + i][col + j].value == value:
+                return False
+    return True
+
+def order(row, col, puzzle):
+    '''
+    returns the order of the domain to check using the least
+    contraining value heuristic
+    ---------------------------
+    Params:
+        row: The row of the current node
+        col: The col of the current node
+        puzzle: A 2d array of Node objects
+    returns:
+        The domain as a new array in the order to use
+    '''
+    # TODO: Change this to use the least constraining value heuristic
+    return deepcopy(puzzle[row][col].domain)
+
+    # order = []
+    # for value in node.domain:
+    #     minDomain = 9
+    #     count = 0
+    #     # Row
+    #     col = node.col
+    #     for row in range(9):
+    #         if value in puzzle[row][col].domain:
+    #             minDomain = min(minDomain, len(puzzle[row][col].domain) - 1)
+    #         else:
+    #             minDomain = min(minDomain, len(puzzle[row][col].domain))
+        
+    #     # Column
+    #     row = node.row
+    #     for col in range(9):
+    #         if puzzle[row][col] == value:
+    #             return False
+    #     # Box
+    #     row = (node.row // 3) * 3
+    #     col = (node.col // 3) * 3
+    #     for i in range(3):
+    #         for j in range(3):
+    #             if puzzle[row + i][col + j] == value:
+    #                 return False
+    # return sorted(order, key=lambda x: x[1])
+
+
+def complete(puzzle):
+    '''
+    Checks if the puzzle is solved
+    ---------------------------
+    Params:
+        puzzle: A 2d array of Node objects
+    returns:
+        True if every node has a value, false otherwise
+    '''
+    for i in range(9):
+        for j in range(9):
+            if puzzle[i][j].value == None:
+                return False
+
+    return True
+
+
+def getNextNode(puzzle):
+    '''
+    returns the next node to check using MRV
+    ---------------------------
+    Params:
+        puzzle: A 2d array of Node objects
+    returns:
+        The next node to search
+    '''
+    nextNode = None
+    for row in puzzle:
+        for node in row:
+            if node.value is None and (nextNode is None or len(nextNode.domain) > len(node.domain)):
+                nextNode = node
+
+    return nextNode
+
+def print_board(puzzle, detailed=False):
 
     print('- ' * 13)
     for row in puzzle:
         print('|', end=' ')
         for col in row:
-            print(col.value if col.value else '.', end=' ')
+            if detailed:
+                domain = ''.join(str(x) for x in col.domain)
+                print("[{} ({:9s})]".format(col.value if col.value else '.', domain), end=' ')
+            else:
+                print(col.value if col.value else '.', end=' ')
             if col.col % 3 == 2:
                 print('|', end=' ')
         print()
@@ -177,7 +401,22 @@ if __name__ == "__main__":
     print()
     print("=========Solved===========")
     print()
-    finishedPuzzle, completed = AC3(p)
-    if (completed):
+    finishedPuzzle, completed, noSolution = AC3(p)
+    if completed:
         print("Sudoku solved")
-    print_board(finishedPuzzle)
+        print_board(finishedPuzzle)
+    elif noSolution:
+        print('No Solution!')
+    else:
+        print('Board after AC3')
+        print_board(finishedPuzzle)
+        print('Starting Backtracking')
+
+        finishedPuzzle, finished = backtrackSearch(finishedPuzzle)
+        print('Board after Backtracking')
+        print('Solved =', finished)
+        print_board(finishedPuzzle)
+
+    print('Actual Solution')
+    print_board(s)
+    
