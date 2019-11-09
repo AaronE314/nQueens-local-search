8b5834cb400e480f770ebbcf8124f8619d5be2df
Aaron <aaronde314@gmail.com>
2019-11-09 18:08:29 -0500

    added 3rd heristic and cleaned up output

--- A2/A2.py
+++ A2/A2.py
@@ -1,8 +1,9 @@
 from queue import Queue
-from copy import 
+from copy import deepcopy
+import sys
+import os
 #for the graph at the end
 import matplotlib.pyplot as plt
-qlengths=[]
 
 class Node:
     def __init__(self,row,col,value=None, domain=range(1, 10)):
@@ -135,7 +136,7 @@ def AC3(puzzle):
 
 
 
-def loadPuzzle(file='./puzzles/easy.csv', num=1, header=True, start=0):
+def loadPuzzle(file='./puzzles/easy.csv', num=1, header=True, start=0, givenSolutions=False):
     '''
     Loads a puzzle from a file
     ---------------------------
@@ -151,7 +152,7 @@ def loadPuzzle(file='./puzzles/easy.csv', num=1, header=True, start=0):
     '''
     with open(file, 'r') as f:
         
-        if num > 1:
+        if num == -1 or num > 1:
             puzzles = []
             solutions = []
 
@@ -163,11 +164,14 @@ def loadPuzzle(file='./puzzles/easy.csv', num=1, header=True, start=0):
             if i < start:
                 continue
         
-            if i > start + num:
+            if num != -1 and i > start + num:
                 break
-
-            puzzleAndSol = line.split(',')
-
+            
+            if givenSolutions:
+                puzzleAndSol = line.split(',')
+            else:
+                line = line.replace(',', '')
+                puzzleAndSol = [line]
             puzzle = []
             for j in range(9):
                 row = []
@@ -175,20 +179,22 @@ def loadPuzzle(file='./puzzles/easy.csv', num=1, header=True, start=0):
                     row.append(Node(j, k, None if puzzleAndSol[0][j*(9) + k] == '.' else puzzleAndSol[0][j*(9) + k]))
                 puzzle.append(row)
 
-            solution = []
-            for j in range(9):
-                row = []
-                for k in range(9):
-                    row.append(Node(j, k, None if puzzleAndSol[1][j*(9) + k] == '.' else puzzleAndSol[1][j*(9) + k]))
-                solution.append(row)
+            if givenSolutions:
+                solution = []
+                for j in range(9):
+                    row = []
+                    for k in range(9):
+                        row.append(Node(j, k, None if puzzleAndSol[1][j*(9) + k] == '.' else puzzleAndSol[1][j*(9) + k]))
+                    solution.append(row)
 
-            if num > 1:
+            if num == -1 or num > 1:
                 puzzles.append(puzzle)
-                solutions.append(solution)
-    if num > 1:
+                if givenSolutions:
+                    solutions.append(solution)
+    if num == -1 or num > 1:
         return puzzles, solutions
     else:
-        return puzzle, solution
+        return puzzle, solution if givenSolutions else None
 
 def backtrackSearch(puzzle):
     '''
@@ -203,6 +209,8 @@ def backtrackSearch(puzzle):
 
     # Find the starting node based on the degree heuristic
     # ie selecting the node with largest amount of constraints
+    # since that node will have the largest degree as there will
+    # be the most unassigned variables around it.
     firstNode = None
     for row in puzzle:
         for node in row:
@@ -240,7 +248,7 @@ def backtrack(puzzle, row, col):
         # with the constraints of the sudoku
         # Should always be consisitant
         if valid(puzzle, row, col, value):
-            
+
             # Store a copy of the current state for
             # the backtracking
             state = deepcopy(puzzle)
@@ -329,34 +337,41 @@ def order(row, col, puzzle):
     returns:
         The domain as a new array in the order to use
     '''
-    # TODO: Change this to use the least constraining value heuristic
-    return deepcopy(puzzle[row][col].domain)
 
-    # order = []
-    # for value in node.domain:
-    #     minDomain = 9
-    #     count = 0
-    #     # Row
-    #     col = node.col
-    #     for row in range(9):
-    #         if value in puzzle[row][col].domain:
-    #             minDomain = min(minDomain, len(puzzle[row][col].domain) - 1)
-    #         else:
-    #             minDomain = min(minDomain, len(puzzle[row][col].domain))
+    node = puzzle[row][col]
+
+    order = []
+    for value in node.domain:
+        affectedValues = 0
+
+        boxRow = (node.row // 3) * 3
+        boxCol = (node.col // 3) * 3
+        # Row
+        col = node.col
+        for row in range(9):
+            if boxRow <= row < boxRow + 3:
+                continue
+            if value in puzzle[row][col].domain:
+                affectedValues += 1
         
-    #     # Column
-    #     row = node.row
-    #     for col in range(9):
-    #         if puzzle[row][col] == value:
-    #             return False
-    #     # Box
-    #     row = (node.row // 3) * 3
-    #     col = (node.col // 3) * 3
-    #     for i in range(3):
-    #         for j in range(3):
-    #             if puzzle[row + i][col + j] == value:
-    #                 return False
-    # return sorted(order, key=lambda x: x[1])
+        # Column
+        row = node.row
+        for col in range(9):
+            if boxCol <= col < boxCol + 3:
+                continue
+            if value in puzzle[row][col].domain:
+                affectedValues += 1
+        
+        # Box
+        for i in range(3):
+            for j in range(3):
+                if value in puzzle[boxRow + i][boxCol + j].domain:
+                    affectedValues += 1
+
+        order.append((value, affectedValues))
+        
+    order = sorted(order, key=lambda x: x[1])
+    return [x[0] for x in order]
 
 
 def complete(puzzle):
@@ -409,42 +424,109 @@ def print_board(puzzle, detailed=False):
         if col.row % 3 == 2:
             print('- ' * 13)
 
+def print_board_and_sol(puzzle, solution):
+
+    print("{:^25s}{:20s}{:^25s}".format("Original Puzzle", "", "Solved After AC3"))
+    print('-' * 25, end='')
+    print(' ' * 20, end='')
+    print('-' * 25)
+    for rowp, rows in zip(puzzle, solution):
+        print('|', end=' ')
+        for col in rowp:
+            print(col.value if col.value else '.', end=' ')
+            if col.col % 3 == 2:
+                print('|', end=' ')
+
+        print(' ' * 19, end='| ')
+        for col in rows:
+            print(col.value if col.value else '.', end=' ')
+            if col.col % 3 == 2:
+                print('|', end=' ')
+        print()
+        if col.row % 3 == 2:
+            print('-' * 25, end='')
+            print(' ' * 20, end='')
+            print('-' * 25)
+
+def print_board_and_sol_and_ac3(puzzle, ac3, solution):
+
+    print("{:^25s}{:20s}{:^25s}{:20s}{:^25s}".format("Original Puzzle", "", "After AC3", "", "Solution"))
+    print('-' * 25, end='')
+    print(' ' * 20, end='')
+    print('-' * 25, end='')
+    print(' ' * 20, end='')
+    print('-' * 25)
+    for rowp, rowac, rows in zip(puzzle, ac3, solution):
+        print('|', end=' ')
+        for col in rowp:
+            print(col.value if col.value else '.', end=' ')
+            if col.col % 3 == 2:
+                print('|', end=' ')
+
+        print(' ' * 19, end='| ')
+        for col in rowac:
+            print(col.value if col.value else '.', end=' ')
+            if col.col % 3 == 2:
+                print('|', end=' ')
+
+        print(' ' * 19, end='| ')
+        for col in rows:
+            print(col.value if col.value else '.', end=' ')
+            if col.col % 3 == 2:
+                print('|', end=' ')
+        print()
+        if col.row % 3 == 2:
+            print('-' * 25, end='')
+            print(' ' * 20, end='')
+            print('-' * 25, end='')
+            print(' ' * 20, end='')
+            print('-' * 25)
+
+qlengths = []
 if __name__ == "__main__":
-    global qlengths
     q = Queue()
-    p, s = loadPuzzle()
-    # addNeighbours(q, p[0][0], p)
-    # print(q.qsize())
-    # while not q.empty():
-    #     print(q.get_nowait())
-    print_board(p)
 
-    print()
-    print("=========Solved===========")
-    print()
-    finishedPuzzle, completed, noSolution = AC3(p)
-    if completed:
-        print("Sudoku solved")
-        print_board(finishedPuzzle)
-    elif noSolution:
-        print('No Solution!')
-    else:
-        print('Board after AC3')
-        print_board(finishedPuzzle)
-        print('Starting Backtracking')
+    num = int(sys.argv[1]) if len(sys.argv) > 1 else 1
 
-        finishedPuzzle, finished = backtrackSearch(finishedPuzzle)
-        print('Board after Backtracking')
-        print('Solved =', finished)
-        print_board(finishedPuzzle)
+    puzzles, _ = loadPuzzle(file='./puzzles/multipleSolutions.txt',num=num, header=False)
 
-    print('Actual Solution')
-    #ploting the queu lengths
-    plt.plot(qlengths)
-    plt.ylabel('Length of the Queue')
-    plt.xlabel('Step count')
-    plt.show()
-    plt.savefig('Sudoku-Queue-length-plot-.png')
-    #
-    print_board(s)
-    
+    if num == 1:
+        puzzles = [puzzles]
+
+    for i, p in enumerate(puzzles, start=1):
+        qlengths = []
+        
+        original_puzzle = deepcopy(p)
+
+        finishedPuzzle, completed, noSolution = AC3(p)
+
+        if len(puzzles) > 1:
+            print("{}:".format(i))
+
+        if completed:
+            print("Sudoku solved using AC3")
+            print_board_and_sol(original_puzzle, finishedPuzzle)
+        elif noSolution:
+            print('No Solution!')
+            print_board(original_puzzle)
+        else:
+
+            print('Board used Backtracking')
+            ac3_puzzle = deepcopy(finishedPuzzle)
+            finishedPuzzle2, finished = backtrackSearch(finishedPuzzle)
+            print_board_and_sol_and_ac3(original_puzzle, ac3_puzzle, finishedPuzzle2)
+            if not finished:
+                print("Backtracking Failed to find a solution")
+                print_board(finishedPuzzle2)
+
+        #ploting the queue lengths
+        if not os.path.exists('queue_lengths'):
+            os.makedirs('queue_lengths')
+        plt.plot(qlengths)
+        plt.xlim(left=0)
+        plt.ylim(bottom=0)
+        plt.ylabel('Length of the Queue')
+        plt.xlabel('Step count')
+        plt.savefig('queue_lengths/Sudoku-Queue-length-plot-{}.png'.format(i))
+        # plt.show()
+        plt.close()
